30cc2ff43c793f86c777cdefd34792a41cd2171c
Praveen kumar S <praveen0031kumar@gmail.com>
2025-10-10 15:51:41 +0530

    project progress

--- src/assets/PortfolioData.js
+++ src/assets/PortfolioData.js
@@ -1,7 +1,15 @@
+import pic1 from './pics/filimin.jpeg'
+import pic2 from './pics/bankmanagement.jpeg'
+import pic3 from './pics/chat.jpeg'
+import pic4 from './pics/weather.jpeg'
+import pic5 from './pics/todos.jpeg'
+import pic6 from './pics/videocall.jpeg'
 export const projects=[
-     { id: 1, title: "E-commerce Site", tech: "React, Node",des:"kahyb" },
-  { id: 2, title: "Blog Platform", tech: "Gatsby, Markdown",des:"kahyb" },
-{ id: 3, title: "Platform", tech: "java, Markdown",des:"maskdiuan" },];
+     { id: 1, title: "Filimin", tech: "Html,Css,Js,Firebase",des:"kahyb",url:pic1,link:"https://github.com/praveenkumar0031/filim.in" },
+  { id: 2, title: "Blog Platform", tech: "Gatsby, Markdown",des:"kahyb",url:pic2},
+{ id: 3, title: "Platform", tech: "java, Markdown",des:"maskdiuan",url:pic3,link:"https://github.com/praveenkumar0031/filim.in" },
+{ id: 4, title: "Weather app", tech: "React.js,open-Api",des:"maskdiuan",url:pic4,link:"https://github.com/praveenkumar0031/filim.in" },
+];
 export function getProjectCount() {
   return projects.length;
 }
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,18 +1,24 @@
+import './project.css' ;
 
 import { projects,getProjectCount } from "../../assets/PortfolioData"
 function Project(){
     const count=getProjectCount();
     return(
-        <div id="project">
-      <h2>My Projects ({count})</h2>
+      <div className='project-seg'>
+      <h2 className='my-project'>My Projects ({count})</h2>
+        <div id="projects" className='projects'>
+      
       {projects.map(project => (
         <div key={project.id}>
-          <h3>{project.title}</h3>
-          
-          <p>Technologies: {project.tech}</p>
-          <p>Descrption: {project.des}</p>
+          <h3 className='title'>{project.title}</h3>
+          <img className='thumbnail' src={project.url} />
+          <p className='tech'>Technologies: {project.tech}</p>
+          <p className='des'>Descrption: {project.des}</p>
+          <p className='link'><a href={project.link} target='_blank'>watch out</a></p>
+          {project.link  && (<p className='live-link'><a href={project.link} target='_blank' rel='noopener noreferrer' >live</a></p>)}
         </div>
       ))}
+        </div>
         </div>
     )
 }
